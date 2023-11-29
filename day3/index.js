7995a6e6e2d9715f7e22b51a324508d678d39a08
Fred Salaysay <fred.salaysay@speakap.nl>
2023-11-29 21:54:27 +0000

    day3.1

--- day3/index.js
+++ day3/index.js
@@ -2,10 +2,39 @@ const { readInput } = require("../readInput")
 
 const input = readInput("day3").split("\n")
 
-const sack = input.map(i => [
-    i.substring(0, i.length / 2),
-    i.substring(i.length / 2, i.length - 1)
-])
+const letters = "abcdefghijklmnopqrstuvwxyz"
+const lower = letters.split("")
+const upper = letters.toUpperCase().split("")
+
+const guide = [].concat(
+    lower.map(
+        (x, index) => [x, index + 1]
+    ),
+).concat(
+    upper.map(
+        (x, index) => [x, index + 1 + 26]
+    )
+)
+
+const duplicate = input.map(i => [
+    i.trim().substring(0, i.length / 2),
+    i.trim().substring(i.length / 2, i.length)
+]).map(
+    (sack) => [ ...new Set(sack[0])].find(
+        y => [ ...new Set(sack[1])].some(z => y === z)
+    )
+).map(
+    dup => guide.find(x => x[0] === dup ? x[1] : false)
+).reduce(
+    (acc, val) => acc += val[1],
+    0
+)
+
+// console.log(sack.map(x => console.log(x[0], x[0].length, x[1],x[1].length)))
+// console.log(sack.map(x => x[0].length === x[1].length))
 
 console.log("========== DAY 3 ==========")
-console.log(sack);
+console.log(`Duplicates: ${duplicate}`);
+// console.log(guide);
+// console.log("PlMsdjPdGMjdPSrSjgddbLb".length)
+// console.log("mHHTszHZzpHmsTFvmpzZzm".length)
