6a23f37c754c3e0f07c962b450bf6e47dd3c4ce3
Fred Salaysay <fred.salaysay@speakap.nl>
2023-11-29 22:08:43 +0000

    changes: add `prettier`, fix formatting, add `gitignore`

--- day1/index.js
+++ day1/index.js
@@ -1,52 +1,51 @@
-const { readInput } = require("../readInput")
+const { readInput } = require("../readInput");
 
-const input = readInput("day1")
+const input = readInput("day1");
 
-let index
-let max = 0
-const elves = input.split("\n\n")
-let list = []
-let topTotal = 0
+let index;
+let max = 0;
+const elves = input.split("\n\n");
+let list = [];
+let topTotal = 0;
 
-for (let i = 0; i < elves.length; i++ ) {
-    let total = 0
-    const meals = elves[i].split("\n")
+for (let i = 0; i < elves.length; i++) {
+    let total = 0;
+    const meals = elves[i].split("\n");
     for (const meal of meals) {
-        total += Number(meal)
-        
-        if (total > max) {
-            max = total
-            index = i
-        }
+        total += Number(meal);
 
+        if (total > max) {
+            max = total;
+            index = i;
+        }
     }
 
     if (list.length === 0) {
-        list.push([ i, total ])
+        list.push([i, total]);
     } else if (list.length < 3) {
-        list.push([ i, total ])
+        list.push([i, total]);
     } else {
         for (let y = 0; y < 3; y++) {
             if (list[y][1] < total) {
-                list.splice(y, 1, [ i, total ])
-                break
+                list.splice(y, 1, [i, total]);
+                break;
             }
         }
     }
 }
 
-list.forEach(map => {
-    console.log(`Adding ${map[1]} to ${topTotal}`)
-    topTotal += map[1]
-})
+list.forEach((map) => {
+    console.log(`Adding ${map[1]} to ${topTotal}`);
+    topTotal += map[1];
+});
 
-console.log("========== DAY 1 ==========")
-console.log("---------- Part One ----------")
-console.log(`Elf #${index} is carrying ${max} calories.`)
-console.log("\n")
-console.log("---------- Part Two ----------")
-console.log(`Elf #${list[0][0]} is carrying ${list[0][1]} calories.`)
-console.log(`Elf #${list[1][0]} is carrying ${list[1][1]} calories.`)
-console.log(`Elf #${list[2][0]} is carrying ${list[2][1]} calories.`)
-console.log(`They are carrying a total of ${topTotal} calories.`)
-console.log(list)
+console.log("========== DAY 1 ==========");
+console.log("---------- Part One ----------");
+console.log(`Elf #${index} is carrying ${max} calories.`);
+console.log("\n");
+console.log("---------- Part Two ----------");
+console.log(`Elf #${list[0][0]} is carrying ${list[0][1]} calories.`);
+console.log(`Elf #${list[1][0]} is carrying ${list[1][1]} calories.`);
+console.log(`Elf #${list[2][0]} is carrying ${list[2][1]} calories.`);
+console.log(`They are carrying a total of ${topTotal} calories.`);
+console.log(list);
--- day2/index.js
+++ day2/index.js
@@ -1,63 +1,67 @@
-const { readInput } = require("../readInput")
+const { readInput } = require("../readInput");
 
-const input = readInput("day2")
+const input = readInput("day2");
 
 // A 1 Rock         X 1 Rock        >> Scissors << Paper
 // B 2 Paper        Y 2 Paper       >> Rock << Scissors
 // C 3 Scissors     Z 3 Scissors    >> Paper << Rock
 const iGuide = new Map();
 
-iGuide.set("A>Z", 3) // 3 + 0
-iGuide.set("A<Y", 8) // 2 + 6
-iGuide.set("A=X", 4) // 3 + 1
-iGuide.set("B>X", 1) // 1 + 0
-iGuide.set("B=Y", 5) // 2 + 3
-iGuide.set("B<Z", 9) // 3 + 6
-iGuide.set("C>Y", 2) // 2 + 0
-iGuide.set("C<X", 7) // 1 + 6
-iGuide.set("C=Z", 6) // 3 + 3
+iGuide.set("A>Z", 3); // 3 + 0
+iGuide.set("A<Y", 8); // 2 + 6
+iGuide.set("A=X", 4); // 3 + 1
+iGuide.set("B>X", 1); // 1 + 0
+iGuide.set("B=Y", 5); // 2 + 3
+iGuide.set("B<Z", 9); // 3 + 6
+iGuide.set("C>Y", 2); // 2 + 0
+iGuide.set("C<X", 7); // 1 + 6
+iGuide.set("C=Z", 6); // 3 + 3
 
 const fGuide = new Map();
 
 // X - lose - 0
 // Y - draw - 3
 // Z - win - 6
-fGuide.set("A X", 3) // 3 + 0
-fGuide.set("A Y", 4) // 1 + 3
-fGuide.set("A Z", 8) // 2 + 6
-fGuide.set("B X", 1) // 1 + 0
-fGuide.set("B Y", 5) // 2 + 3
-fGuide.set("B Z", 9) // 3 + 6
-fGuide.set("C X", 2) // 2 + 0
-fGuide.set("C Y", 6) // 3 + 3
-fGuide.set("C Z", 7) // 1 + 6
+fGuide.set("A X", 3); // 3 + 0
+fGuide.set("A Y", 4); // 1 + 3
+fGuide.set("A Z", 8); // 2 + 6
+fGuide.set("B X", 1); // 1 + 0
+fGuide.set("B Y", 5); // 2 + 3
+fGuide.set("B Z", 9); // 3 + 6
+fGuide.set("C X", 2); // 2 + 0
+fGuide.set("C Y", 6); // 3 + 3
+fGuide.set("C Z", 7); // 1 + 6
 
 const run = (input) => {
     const answers = input.split(" ");
     // console.log(answers) // [x, y]
-    const x = answers[0] // opponent
-    const y = answers[1] // me
+    const x = answers[0]; // opponent
+    const y = answers[1]; // me
     // console.log(x, y) // x y
 
-    const key = [ ...iGuide.keys() ].find(v => v.charAt(0) === x && v.charAt(2) === y)
+    const key = [...iGuide.keys()].find(
+        (v) => v.charAt(0) === x && v.charAt(2) === y,
+    );
     return iGuide.get(key);
 };
 
 const run2 = (input) => {
     const answers = input.split(" ");
     // console.log(answers) // [x, y]
-    const x = answers[0] // opponent
-    const y = answers[1] // me
+    const x = answers[0]; // opponent
+    const y = answers[1]; // me
     // console.log(x, y) // x y
 
-    const key = [ ...fGuide.keys() ].find(v => v.charAt(0) === x && v.charAt(2) === y)
+    const key = [...fGuide.keys()].find(
+        (v) => v.charAt(0) === x && v.charAt(2) === y,
+    );
     return fGuide.get(key);
 };
 
-const games = input.split("\n")
-const total = games.map(run).reduce((acc, v) => acc + v, 0)
-const total2 = games.map(run2).reduce((acc, v) => acc + v, 0)
+const games = input.split("\n");
+const total = games.map(run).reduce((acc, v) => acc + v, 0);
+const total2 = games.map(run2).reduce((acc, v) => acc + v, 0);
 
-console.log("========== DAY 2 ==========")
-console.log(`Total: ${total}`)
-console.log(`Total2: ${total2}`)
+console.log("========== DAY 2 ==========");
+console.log(`Total: ${total}`);
+console.log(`Total2: ${total2}`);
